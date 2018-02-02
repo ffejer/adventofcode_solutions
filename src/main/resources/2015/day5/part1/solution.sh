b410e4016b8b07b259c703d64f7941e69d5cdabe
ffejer <Ferenc.Fejer@bwinparty.com>
2018-02-02 14:51:05 +0100

    solutions day 2 and 5 2015

--- src/main/resources/2015/day5/part1/solution.sh
+++ src/main/resources/2015/day5/part1/solution.sh
@@ -7,10 +7,8 @@ grep -E '(ab|cd|pq|xy)' input.txt
 grep -v -E '(ab|cd|pq|xy)' input.txt | grep -E '(.)\1' | grep -E '([aeiou].*){3,}'  | wc -l
 
 # part 2
-
 grep -E '(..).(..)\1' input.txt; #
 grep -E '(.).\1' input.txt; #
-
 
 # final solution
 grep -E '(..).*\1' input.txt | grep -E '(.).\1' | wc -l
