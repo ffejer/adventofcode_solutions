80bda14370fa2c2082e76198ae79bf6bd0a22fbe
Ferenc Fejer <FerencFejer@gmail.com>
2019-12-03 07:21:45 +0100

    day 3 part 1 solution -- wip

--- src/main/java/com/adventofcode/year2018/day3/OverlappingBruteForce.java
+++ src/main/java/com/adventofcode/year2018/day3/OverlappingBruteForce.java
@@ -3,31 +3,46 @@ package com.adventofcode.year2018.day3;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Overlapping {
+public class OverlappingBruteForce {
     private static String REGEXP = "#([0-9]+)\\W@\\W([0-9]+),([0-9]+):\\W([0-9]+)x([0-9]+)";
     private static Pattern pattern = Pattern.compile(REGEXP);
 
     public static void main(String[] args) {
-        Overlapping overlapping = new Overlapping();
+        OverlappingBruteForce overlapping = new OverlappingBruteForce();
         try (Stream<String> stream = Files.lines(Paths.get("./src/main/resources/2018/day/3/input"))) {
-            overlapping.doWork(stream);
+            overlapping.doCalculation(stream);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-
-
-    void doWork(Stream<String> stream) {
+    void doCalculation(Stream<String> stream) {
         List<Claim> collect = stream.map(s -> new Claim(pattern.matcher(s))).collect(Collectors.toList());
+        Map<Point, Integer> overlapping = new HashMap<>();
+        for(Claim claim : collect) {
+            Set<Point> points = claim.getPoints();
+            for(Point point : points) {
+                if(overlapping.containsKey(point)) {
+                    // todo
+                }
+            }
+        }
         return;
+    }
+
+    class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 
     class Claim {
@@ -43,24 +58,16 @@ public class Overlapping {
             this.dy = Integer.parseInt(matcher.group(5));
         }
 
-        public int getId() {
-            return id;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        public int getDx() {
-            return dx;
-        }
-
-        public int getDy() {
-            return dy;
+        Set<Point> getPoints(){
+            Set<Point> result = new HashSet<>();
+            int x2 = this.x + this.dx;
+            int y2 = this.y + this.dy;
+            for(int i = this.x; i < x2; i++) {
+                for (int j = this.y; j < y2; j++) {
+                    result.add(new Point(i,j));
+                }
+            }
+            return result;
         }
     }
 }
