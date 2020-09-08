db99bb0a948c7dd1b53886d2d4df152f6fed3d60
Ferenc Fejer <FerencFejer@gmail.com>
2020-09-08 18:19:28 +0200

    2019 day 4 part 1 & 2

---
+++ src/main/java/com/adventofcode/Pair.java
@@ -0,0 +1,44 @@
+package com.adventofcode;
+
+import java.util.Objects;
+
+public class Pair<T, U> {
+    private T left;
+    private U right;
+
+    public Pair(T left, U right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getLeft() {
+        return left;
+    }
+
+    public U getRight() {
+        return right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return left.equals(pair.left) &&
+                right.equals(pair.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "left=" + left +
+                ", right=" + right +
+                '}';
+    }
+
+}
