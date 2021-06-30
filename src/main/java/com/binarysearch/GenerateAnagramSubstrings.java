cb529e3f977b75904af6b9ecd1d2ef7dd5138d1a
Ferenc Fejér <H50CTYA@s-mxs.net>
2021-06-30 10:41:14 +0200

    binary search GenerateAnagramSubstrings

---
+++ src/main/java/com/binarysearch/GenerateAnagramSubstrings.java
@@ -0,0 +1,13 @@
+package com.binarysearch;
+
+/**
+ * You are given a lowercase alphabet string s. Return all substrings in s where there is another substring in s at a different location that is an anagram. Return the list sorted in lexicographic order.
+ *
+ * Constraints
+ * 1 ≤ n ≤ 100 where n is the length of s
+ */
+public class GenerateAnagramSubstrings {
+  public String[] solve(String s) {
+    return new String[] {};
+  }
+}
