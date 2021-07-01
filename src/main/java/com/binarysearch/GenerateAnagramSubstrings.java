2eb925977b138b8006e10d8902a0964b9ea6bb8a
Ferenc Fejér <H50CTYA@s-mxs.net>
2021-07-01 09:47:31 +0200

    binary search GenerateAnagramSubstrings cleanup

--- src/main/java/com/binarysearch/GenerateAnagramSubstrings.java
+++ src/main/java/com/binarysearch/GenerateAnagramSubstrings.java
@@ -17,7 +17,6 @@ public class GenerateAnagramSubstrings {
 
   public static void main(String[] args) {
     String[] input = new GenerateAnagramSubstrings().solve("aaabcda");
-    System.out.println(Arrays.toString(input));
   }
 
   public String[] solve(String s) {
@@ -35,13 +34,13 @@ public class GenerateAnagramSubstrings {
 
   private Map<String, ArrayList<String>> generateAndCountSubstrings(String s) {
     Map<String, ArrayList<String>> result = new HashMap<>();
-    for (int length = 1; length < s.length(); length++) {
-      for (int start = 0; start < s.length() - length + 1; start++) {
-        String substring = s.substring(start, start + length);
+    for (var length = 1; length < s.length(); length++) {
+      for (var start = 0; start < s.length() - length + 1; start++) {
+        var substring = s.substring(start, start + length);
         char[] chars = substring.toCharArray();
-        String substr = String.valueOf(chars);
+        var substr = String.valueOf(chars);
         Arrays.sort(chars);
-        String key = String.valueOf(chars);
+        var key = String.valueOf(chars);
         ArrayList<String> list = result.getOrDefault(key, new ArrayList<>());
         list.add(substr);
         result.putIfAbsent(key, list);
