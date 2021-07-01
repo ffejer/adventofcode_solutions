e98401fa37147bc4a2d6b07cd24062e311be1bf2
Ferenc Fejér <H50CTYA@s-mxs.net>
2021-07-01 09:46:33 +0200

    binary search GenerateAnagramSubstrings solution from the site

--- src/main/java/com/binarysearch/GenerateAnagramSubstrings.java
+++ src/main/java/com/binarysearch/GenerateAnagramSubstrings.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 /**
  * You are given a lowercase alphabet string s. Return all substrings in s where there is another
@@ -21,45 +21,30 @@ public class GenerateAnagramSubstrings {
   }
 
   public String[] solve(String s) {
-    ArrayList<String> strings = generateSubstrings(s);
-    ArrayList<String> result = new ArrayList<>();
-    System.out.println(strings);
-    List<HashMap<Integer, Integer>> sums =
-        strings.stream().map(this::sumchars).collect(Collectors.toList());
-
-    for (int i = 0; i < strings.size() - 1; i++) {
-      for (int j = i + 1; j < strings.size(); j++) {
-        if (sums.get(i).equals(sums.get(j))) {
-          result.add(strings.get(i));
-          result.add(strings.get(j));
-        }
+    Map<String, ArrayList<String>> strings = generateAndCountSubstrings(s);
+    List<String> result = new ArrayList<>();
+    for (List<String> l : strings.values()) {
+      if (l.size() > 1) {
+        result.addAll(l);
       }
     }
-    String[] returnValue = result.toArray(new String[0]);
-    Arrays.sort(returnValue);
-    return returnValue;
+    String[] retval = result.toArray(new String[0]);
+    Arrays.sort(retval);
+    return retval;
   }
 
-  private HashMap<Integer, Integer> sumchars(String s) {
-    HashMap<Integer, Integer> result = new HashMap<>();
-    for (int i = 0; i < s.length(); i++) {
-      char c = s.charAt(i);
-      if (result.containsKey((int) c)) {
-        Integer integer = result.get((int) c);
-        integer++;
-        result.put((int) c, integer);
-      } else {
-        result.put((int) c, 1);
-      }
-    }
-    return result;
-  }
-
-  private ArrayList<String> generateSubstrings(String s) {
-    ArrayList<String> result = new ArrayList<>();
+  private Map<String, ArrayList<String>> generateAndCountSubstrings(String s) {
+    Map<String, ArrayList<String>> result = new HashMap<>();
     for (int length = 1; length < s.length(); length++) {
       for (int start = 0; start < s.length() - length + 1; start++) {
-        result.add(s.substring(start, start + length));
+        String substring = s.substring(start, start + length);
+        char[] chars = substring.toCharArray();
+        String substr = String.valueOf(chars);
+        Arrays.sort(chars);
+        String key = String.valueOf(chars);
+        ArrayList<String> list = result.getOrDefault(key, new ArrayList<>());
+        list.add(substr);
+        result.putIfAbsent(key, list);
       }
     }
     return result;
