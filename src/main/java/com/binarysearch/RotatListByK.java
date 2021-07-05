9b91b9aee5e28522b5dd77f6cd88b87eb1f6f4b7
Ferenc Fejér <H50CTYA@s-mxs.net>
2021-07-05 15:51:11 +0200

    binary search RotatListByK cleanup

---
+++ src/main/java/com/binarysearch/RotatListByK.java
@@ -0,0 +1,19 @@
+package com.binarysearch;
+
+/**
+ * <pre>Write a function that rotates a list of numbers to the left by k elements. Numbers should wrap around.
+ *
+ * Note: The list is guaranteed to have at least one element, and k is guaranteed to be less than or equal to the length of the list.
+ *
+ * Bonus: Do this without creating a copy of the list. How many swap or move operations do you need?
+ *
+ * Constraints
+ *
+ * n ≤ 100,000 where n is the length of nums
+ * </pre>
+ */
+public class RotatListByK {
+  public int[] solve(int[] nums, int k) {
+    return new int[0];
+  }
+}
