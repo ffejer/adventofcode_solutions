9d90c0ce22a351c8c2942054da84894e3b3563fa
Ferenc Fejér <H50CTYA@s-mxs.net>
2024-11-04 13:08:02 +0100

    delete broken code

--- src/main/kotlin/Main.kt
+++
@@ -1,9 +0,0 @@
-fun sayHello(greeting:String, toGreet:String) = println("$greeting $toGreet")
-
-fun main() {
-    val things = mutableListOf("Kotlin", "programming", "comic books")
-    val map = mapOf(1 to "a", 2 to "b", 3 to "c")
-    map.forEach { (key, value) -> println("$key -> $value") }
-    things.add("Star Trek")
-    things.forEach { println(it) }
-}
