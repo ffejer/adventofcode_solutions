156976d034c3887ed6cba50813ff6b7664bed512
Ferenc Fejér <H50CTYA@s-mxs.net>
2024-11-04 18:05:06 +0100

    solution

---
+++ src/main/java/com/adventofcode/year2022/day4/CampCleanup.java
@@ -0,0 +1,41 @@
+package com.adventofcode.year2022.day4;
+
+import com.adventofcode.util.AbstractProblemSolution;
+import java.text.MessageFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CampCleanup extends AbstractProblemSolution {
+
+  private static final Logger logger = LoggerFactory.getLogger(CampCleanup.class);
+
+  public static void main(String[] args) {
+    new CampCleanup().solve();
+  }
+
+  private void solve() {
+    var input = getInputLines("./src/main/resources/input/2022/4/input");
+    int solution = 0;
+    for (String line : input) {
+      String[] assignments = line.split(",");
+      String[] team1 = assignments[0].split("-");
+      String[] team2 = assignments[1].split("-");
+      if (fullOverlap(Integer.parseInt(team1[0]), Integer.parseInt(team1[1]),
+          Integer.parseInt(team2[0]), Integer.parseInt(team2[1]))) {
+        solution++;
+      }
+    }
+    logger.info(String.valueOf(solution));
+  }
+
+  boolean fullOverlap(int teamAFrom, int teamATo, int teamBFrom, int teamBTo) {
+    if (!(teamAFrom <= teamATo && teamBFrom <= teamBTo)) {
+      throw new IllegalStateException(
+          MessageFormat.format("Sections [{0}, {1}] & [{2}, {3}]", teamAFrom, teamATo,
+              teamBFrom, teamBTo));
+    }
+    return (teamAFrom >= teamBFrom && teamATo <= teamBTo)
+        || (teamBFrom >= teamAFrom && teamBTo <= teamATo);
+  }
+
+}
